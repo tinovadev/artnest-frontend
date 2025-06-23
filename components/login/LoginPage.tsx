25a6a7db40e3e68992a59952173459f2d2c7690f
tinovadev <tinova.dev@gmail.com>
2025-06-23 22:26:34 +0900

    Left Align Brand Elements

--- components/login/LoginPage.tsx
+++ components/login/LoginPage.tsx
@@ -103,15 +103,15 @@ export default function LoginPage() {
 
         {/* Desktop Layout */}
         <div className="hidden lg:flex min-h-screen">
-          {/* Left Side - Logo and Slogan Only */}
-          <div className="flex-1 bg-background flex flex-col justify-center items-center px-12 xl:px-20">
-            <div className="text-center">
+          {/* Left Side - Logo and Slogan Only (Left Aligned) */}
+          <div className="flex-1 bg-background flex flex-col justify-center px-12 xl:px-20">
+            <div className="text-left">
               {/* ArtNest Logo */}
               <div className="mb-16">
                 <img 
                   src="/artnest-logo.svg" 
                   alt="ArtNest Logo" 
-                  className="h-20 xl:h-24 w-auto mx-auto"
+                  className="h-20 xl:h-24 w-auto"
                 />
               </div>
               
@@ -173,14 +173,14 @@ export default function LoginPage() {
                     <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
                     <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
                     <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
-                  </svg>
-                </div>
-                Google Login
-              </Button>
-            </div>
+                </svg>
+              </div>
+              Google Login
+            </Button>
           </div>
         </div>
-      </ScrollArea>
-    </div>
-  );
+      </div>
+    </ScrollArea>
+  </div>
+);
 }
