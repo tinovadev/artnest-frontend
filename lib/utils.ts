8a085764065150a3fb7e0883330b1358b46550bb
icecreamqw <jsyun010518@naver.com>
2025-06-29 12:21:30 +0900

    update me api + page

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,17 @@ export function formatDateToDotFormat(dateString: string): string {
   const day = `${date.getDate()}`.padStart(2, "0");
   return `${year}.${month}.${day}`;
 }
+
+export function camelToSnake(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(v => camelToSnake(v));
+  } else if (obj !== null && typeof obj === 'object') {
+    return Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [
+        key.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`),
+        camelToSnake(value),
+      ])
+    );
+  }
+  return obj;
+}
