825bcdf7ed407f9d62dd0f81d042896d86eb7b1b
tinovadev <tinova.dev@gmail.com>
2025-06-30 22:43:51 +0900

    Fix NextAuth session context error

--- hooks/nextauth-provider.tsx
+++ hooks/nextauth-provider.tsx
@@ -1,8 +1,14 @@
-// app/providers.tsx
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-export function NextAuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function NextAuthProvider({ 
+  children, 
+  session 
+}: { 
+  children: React.ReactNode;
+  session: Session | null;
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
